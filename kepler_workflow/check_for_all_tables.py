8c5129c247e95b4520bbb07c0d0a82e991326b24
Jorge <jorgemarpa@ug.uchile.cl>
2021-10-15 18:39:10 -0700

    show which missing

--- kepler_workflow/check_for_all_tables.py
+++ kepler_workflow/check_for_all_tables.py
@@ -3,15 +3,23 @@ import glob
 import numpy as np
 from paths import ARCHIVE_PATH, PACKAGEDIR
 
+
 def run():
 
     folder_list = np.sort(glob.glob(f"{ARCHIVE_PATH}/data/kepler/tpf/*"))
     print(len(folder_list))
 
     for folder in folder_list:
-        fname = folder.split('/')[-1]
-        table_list = np.sort(glob.glob(f"{PACKAGEDIR}/data/support/kepler_tpf_map_{fname}_q*_tar.csv"))
+        fname = folder.split("/")[-1]
+        table_list = np.sort(
+            glob.glob(f"{PACKAGEDIR}/data/support/kepler_tpf_map_{fname}_q*_tar.csv")
+        )
         print(f"Folder name {fname} totla tables {len(table_list)}")
 
-if __name__ == '__main__':
+        if len(table_list) < 18:
+            print("Q avail: ", [x[-10:-8] for x in table_list])
+
+
+if __name__ == "__main__":
+
     run()
