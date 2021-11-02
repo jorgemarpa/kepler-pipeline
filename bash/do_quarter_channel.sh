236ca64703fde0c54bba0f2a4d1db0065cf44155
Jorge <jorgemarpa@ug.uchile.cl>
2021-11-01 21:49:51 -0700

    working dir fixed

--- bash/do_quarter_channel.sh
+++ bash/do_quarter_channel.sh
@@ -8,9 +8,11 @@ echo "Channel $channel Quarter $quarter"
 quarter=$((quarter + 2))
 channel=$((channel + 1))
 
-tpftot=`head -"$quarter" /Users/jorgemarpa/Work/BAERI/ADAP/kepler-workflow/data/support/kepler_ntpf_qch.csv | tail -1 | cut -f "$channel" -d","`
-bsize=`head -"$quarter" /Users/jorgemarpa/Work/BAERI/ADAP/kepler-workflow/data/support/kepler_batchsize_qch.csv | tail -1 | cut -f "$channel" -d","`
-bntot=`head -"$quarter" /Users/jorgemarpa/Work/BAERI/ADAP/kepler-workflow/data/support/kepler_nbatches_qch.csv | tail -1 | cut -f "$channel" -d","`
+WORKDIR=$(dirname `pwd`)
+
+tpftot=`head -"$quarter" "$WORKDIR"/data/support/kepler_ntpf_qch.csv | tail -1 | cut -f "$channel" -d","`
+bsize=`head -"$quarter" "$WORKDIR"/data/support/kepler_batchsize_qch.csv | tail -1 | cut -f "$channel" -d","`
+bntot=`head -"$quarter" "$WORKDIR"/data/support/kepler_nbatches_qch.csv | tail -1 | cut -f "$channel" -d","`
 
 echo "Total TPFs $tpftot"
 echo "$bntot batches of size $bsize"
@@ -27,4 +29,4 @@ fi
 hh="00"
 mm="01"
 
-echo qsub -J 0-${bntot} -lwalltime=${hh}:${mm}:00 do_qch.sh ${quarter} ${channel}
+echo qsub -J 0-${bntot} -lwalltime=${hh}:${mm}:00 ${WORKDIR}/kepler-workflow/do_quarter_channel.sh ${quarter} ${channel}
