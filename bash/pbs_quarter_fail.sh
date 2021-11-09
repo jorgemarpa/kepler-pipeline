ad71746f9f27920c059564593aabbc761497f066
Jorge <jorgemarpa@ug.uchile.cl>
2021-11-08 18:29:08 -0800

    script to do failed batches

---
+++ bash/pbs_quarter_fail.sh
@@ -0,0 +1,34 @@
+#PBS -S /bin/sh
+#PBS -N make-LCs
+#PBS -q normal
+#PBS -l select=1:ncpus=20:mem=60G:model=ivy
+#PBS -l walltime=07:00:00
+#PBS -j oe
+#PBS -m e
+#PBS -V
+#PBS -o pfe21:/home4/jimartin/ADAP/kepler-workflow/logs/
+
+# activate conda env
+source /nasa/jupyter/4.4/miniconda/etc/profile.d/conda.sh
+conda activate kepler-workflow
+
+# project directory
+WORKDIR=$(dirname `pwd`)
+
+# get batch info from quarter file
+info="${WORKDIR}/data/support/fail_batch_index_quarter${quarter}.dat"
+totallines=`cat "$info" | wc -l | sed 's/^ *//g'`
+
+# change directory
+cd "$WORKDIR/kepler_workflow"
+echo `pwd`
+
+echo "Quarter $quarter"
+echo "Total batches in quarter $totallines"
+
+# lunch parallel jobs
+echo "Will run the following command:"
+echo "cat ${info} | xargs -n 1 -I {} -P 20 python make_lightcurves.py --quarter ${quarter} --batch-index {} --tar-tpfs --tar-lcs --fit-va --log 20"
+cat ${info} | xargs -n 1 -I {} -P 20 echo "python make_lightcurves.py --quarter ${quarter} --batch-index {} --tar-tpfs --tar-lcs --fit-va --log 20 --dry-run"
+
+exit 0
