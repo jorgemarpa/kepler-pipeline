4c44185d0db7673bf8b8f6334227d90e026c4900
Jorge <jorgemarpa@ug.uchile.cl>
2022-01-07 18:17:54 -0300

    echos and exports

--- bash/pbs_quarter_channel.sh
+++ bash/pbs_quarter_channel.sh
@@ -8,15 +8,25 @@
 #PBS -V
 #PBS -o pfe21:/home4/jimartin/ADAP/kepler-workflow/logs/
 
-# change directory
-cd /home4/jimartin/ADAP/kepler-workflow/kepler_workflow
-
 # activate conda env
 source /nasa/jupyter/4.4/miniconda/etc/profile.d/conda.sh
 conda activate kepler-workflow
+
+# project directory
+WORKDIR=$(dirname `pwd`)
+
+# change directory
+cd "$WORKDIR/kepler_workflow"
+echo `pwd`
+
+
 
 echo "Channel $ch Quarter $qu"
 echo "$bn batches of size $bs"
 
 # lunch parallel jobs
+echo "Will run the following command:"
+echo "seq 1 ${bn} | xargs -n 1 -I {} -P 10 python make_lightcurves_new.py --quarter ${qu} --channel ${ch} --batch-size ${bs} --batch-number {} --tar-tpfs --tar-lcs --augment-bkg --plot --fit-va --log 20"
 seq 1 ${bn} | xargs -n 1 -I {} -P 10 python make_lightcurves_new.py --quarter ${qu} --channel ${ch} --batch-size ${bs} --batch-number {} --tar-tpfs --tar-lcs --augment-bkg --plot --fit-va --log 20
+
+exit 0
