03d18a3ee7ca66a90a1d245d489a5f5af447ea5c
Jorge <jorgemarpa@ug.uchile.cl>
2022-01-13 16:18:32 -0300

    fail pbs fixed

--- bash/pbs_quarter_fail.sh
+++ bash/pbs_quarter_fail.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/sh
 #PBS -N make-LCs
 #PBS -q normal
-#PBS -l select=1:ncpus=15:mem=60G:model=ivy
+#PBS -l select=1:ncpus=10:mem=60G:model=ivy
 #PBS -l walltime=02:00:00
 #PBS -j oe
 #PBS -m e
@@ -14,7 +14,6 @@ conda activate kepler-workflow
 
 # project directory
 WORKDIR=$(dirname `pwd`)
-quarter=5
 
 # get batch info from quarter file
 info="${WORKDIR}/data/support/fail_batch_index_quarter${quarter}.dat"
@@ -29,7 +28,7 @@ echo "Total batches in quarter $totallines"
 
 # lunch parallel jobs
 echo "Will run the following command:"
-echo "cat ${info} | xargs -n 1 -I {} -P 15 python make_lightcurves.py --quarter ${quarter} --batch-index {} --tar-tpfs --tar-lcs --fit-va --augment-bkg --log 20"
-cat ${info} | xargs -n 1 -I {} -P 15 python make_lightcurves.py --quarter ${quarter} --batch-index {} --tar-tpfs --tar-lcs --fit-va --augment-bkg --log 20
+echo "cat ${info} | xargs -n 1 -I {} -P 10 python make_lightcurves_new.py --quarter ${quarter} --batch-index {} --tar-tpfs --tar-lcs --fit-va --augment-bkg --log 20"
+cat ${info} | xargs -n 1 -I {} -P 10 python make_lightcurves_new.py --quarter ${quarter} --batch-index {} --tar-tpfs --tar-lcs --fit-va --augment-bkg --log 20
 
 exit 0
