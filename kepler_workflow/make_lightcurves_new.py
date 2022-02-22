d719b9ae538d0d7cac9930dfae592a9dd8206e0e
Jorge <jorgemarpa@ug.uchile.cl>
2022-02-21 22:26:18 -0800

    save npz

--- kepler_workflow/make_lightcurves_new.py
+++ kepler_workflow/make_lightcurves_new.py
@@ -613,27 +613,24 @@ def do_lcs(
     logg.info(f"Saving light curves into: {dir_name}")
     if not os.path.isdir(dir_name):
         os.makedirs(dir_name)
+    tarf_name = "%s/kbonus-bkgd_ch%02i_q%02i_v%s_lc_b%03i-%02i_%s_%s_tk%i_tp%i_fva%s_bkg%s_aug%s.tar.gz" % (
+        dir_name,
+        channel,
+        quarter,
+        lc_version,
+        batch_size,
+        batch_number,
+        machine.time_corrector.replace("_", ""),
+        machine.cartesian_knot_spacing,
+        machine.n_time_knots,
+        machine.n_time_points,
+        str(fit_va)[0],
+        str(config["renormalize_tpf_bkg"])[0],
+        str(augment_bkg)[0],
+    )
     if tar_lcs:
         logg.info("LCFs will be tar.gz")
-        tar = tarfile.open(
-            "%s/kbonus-bkgd_ch%02i_q%02i_v%s_lc_b%03i-%02i_%s_%s_tk%i_tp%i_fva%s_bkg%s_aug%s.tar.gz"
-            % (
-                dir_name,
-                channel,
-                quarter,
-                lc_version,
-                batch_size,
-                batch_number,
-                machine.time_corrector.replace("_", ""),
-                machine.cartesian_knot_spacing,
-                machine.n_time_knots,
-                machine.n_time_points,
-                str(fit_va)[0],
-                str(config["renormalize_tpf_bkg"])[0],
-                str(augment_bkg)[0],
-            ),
-            mode="w:gz",
-        )
+        tar = tarfile.open(tarf_name, mode="w:gz")
     for idx, srow in tqdm(
         machine.sources.iterrows(),
         total=machine.nsources,
@@ -707,11 +704,14 @@ def do_lcs(
 
     if save_npy:
         np.savez(
-            tar.replace("tar.gz", "npz"),
+            tarf_name.replace("tar.gz", "npz"),
             time=machine.time,
             flux=machine.flux,
             flux_err=machine.flux_err,
-            source=machine.sources.designation.values,
+            sap_flux=machine.sap_flux,
+            sap_flux_err=machine.sap_flux_err,
+            chi2_lc=chi2_lc,
+            sources=machine.sources.designation.values,
         )
 
 
