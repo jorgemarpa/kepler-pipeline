42b6d9b3c162ab9a24880ba2b0326e6c2de5a719
Jorge <jorgemarpa@ug.uchile.cl>
2022-03-18 15:57:59 -0700

    adding segments

--- kepler_workflow/make_lightcurves_new.py
+++ kepler_workflow/make_lightcurves_new.py
@@ -470,7 +470,10 @@ def do_lcs(
         aperture_size="optimal", target_complete=1, target_crowd=1
     )
     # PSF phot
-    machine.build_time_model(plot=False)
+    split_time_model = True
+    logg.info(f"split_time_model: {split_time_model}")
+    machine.build_time_model(plot=False, split_time_model=split_time_model)
+    logg.info(f"Time model segment splits: {[x for x in machine.seg_splits]}")
     logg.info("Fitting models...")
     machine.fit_model(fit_va=fit_va)
     iter_negative = False
@@ -541,18 +544,22 @@ def do_lcs(
         logg.info(f"Saving diagnostic plots into: {dir_name}")
         if not os.path.isdir(dir_name):
             os.makedirs(dir_name)
-        file_name = "%s/%s_models_ch%02i_q%02i_b%03i-%02i_%s_%s_tk%i_tp%i_bkg%s.pdf" % (
-            dir_name,
-            machine.tpf_meta["mission"][0],
-            channel,
-            quarter,
-            batch_size,
-            batch_number,
-            machine.time_corrector.replace("_", ""),
-            machine.cartesian_knot_spacing,
-            machine.n_time_knots,
-            machine.n_time_points,
-            str(config["renormalize_tpf_bkg"])[0],
+        file_name = (
+            "%s/%s_models_ch%02i_q%02i_b%03i-%02i_%s_%s_tk%i_tp%i_bkg%s_sgm%s.pdf"
+            % (
+                dir_name,
+                machine.tpf_meta["mission"][0],
+                channel,
+                quarter,
+                batch_size,
+                batch_number,
+                machine.time_corrector.replace("_", ""),
+                machine.cartesian_knot_spacing,
+                machine.n_time_knots,
+                machine.n_time_points,
+                str(config["renormalize_tpf_bkg"])[0],
+                str(split_time_model)[0],
+            )
         )
 
         # bkg model
@@ -613,7 +620,7 @@ def do_lcs(
     logg.info(f"Saving light curves into: {dir_name}")
     if not os.path.isdir(dir_name):
         os.makedirs(dir_name)
-    tarf_name = "%s/kbonus-bkgd_ch%02i_q%02i_v%s_lc_b%03i-%02i_%s_%s_tk%i_tp%i_fva%s_bkg%s_aug%s.tar.gz" % (
+    tarf_name = "%s/kbonus-bkgd_ch%02i_q%02i_v%s_lc_b%03i-%02i_%s_%s_tk%i_tp%i_fva%s_bkg%s_aug%s_sgm%s.tar.gz" % (
         dir_name,
         channel,
         quarter,
@@ -627,6 +634,7 @@ def do_lcs(
         str(fit_va)[0],
         str(config["renormalize_tpf_bkg"])[0],
         str(augment_bkg)[0],
+        str(split_time_model)[0],
     )
     if tar_lcs:
         logg.info("LCFs will be tar.gz")
