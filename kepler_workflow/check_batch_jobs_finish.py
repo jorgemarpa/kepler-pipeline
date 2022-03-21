d7c4884b5eb0da1792c79d103212b6a85437c02c
Jorge <jorgemarpa@ug.uchile.cl>
2022-03-21 16:01:11 -0700

    check archive

--- kepler_workflow/check_batch_jobs_finish.py
+++ kepler_workflow/check_batch_jobs_finish.py
@@ -1,9 +1,12 @@
 import os
 from glob import glob
-from paths import PACKAGEDIR
+import argparse
+import numpy as np
+
+from paths import ARCHIVE_PATH, OUTPUT_PATH, LCS_PATH, PACKAGEDIR
 
 
-def main():
+def check_make_files():
     info_list = sorted(glob(f"{PACKAGEDIR}/logs/make_lightcurve_*.info"))
     print(f"Total info files: {len(info_list)}")
 
@@ -27,7 +30,45 @@ def main():
     ) as f:
         for k in batch_idx_fail:
             f.write(f"{k}\n")
+    return
+
+
+def check_channel_archive(
+    channel, pattern="polynomial_sqrt_tk6_tp100_fvaT_bkgT_augT_sgmT"
+):
+
+    quarters = np.arange(0, 18)
+    for q in quarters:
+        archive_path = sorted(
+            glob(f"{LCS_PATH}/kepler/ch{channel:02}/q{q:02}/*{pattern}*")
+        )
+        if len(archive_path) > 0:
+            total_batches = archive_path[0].split("/")[-1][34:36]
+        else:
+            total_batches = None
+        print(f"Channel {channel} Q {q} batches {len(archive_path)}")
+    return
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="AutoEncoder")
+    parser.add_argument(
+        "--mode",
+        dest="mode",
+        default="make_files",
+        help="Which type of files to check.",
+    )
+    parser.add_argument(
+        "--channel",
+        dest="channel",
+        type=int,
+        default=None,
+        help="Channel channel",
+    )
+    args = parser.parse_args()
+    kwargs = vars(args)
+
+    if args.mode == "make_files":
+        check_make_files()
+    elif args.mode == "check_archive":
+        check_channel_archive(args.channel)
