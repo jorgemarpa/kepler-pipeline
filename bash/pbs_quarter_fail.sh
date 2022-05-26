4d4f7db9113523957a2bb9ee4a1292fb24e735e3
Jorge <jorgemarpa@ug.uchile.cl>
2022-05-26 11:03:21 -0700

    ivy

--- bash/pbs_quarter_fail.sh
+++ bash/pbs_quarter_fail.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/sh
 #PBS -N make-LCs
 #PBS -q normal
-#PBS -l select=1:ncpus=5:mem=126G:model=has
+#PBS -l select=1:ncpus=5:mem=62G:model=ivy
 #PBS -l walltime=06:00:00
 #PBS -j oe
 #PBS -m e
