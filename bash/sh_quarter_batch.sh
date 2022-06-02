f56e2d04e2a78732168d9757468dd55860d36ef2
Jorge <jorgemarpa@ug.uchile.cl>
2022-06-01 23:34:40 -0700

    batches of 50

--- bash/sh_quarter_batch.sh
+++ bash/sh_quarter_batch.sh
@@ -12,15 +12,15 @@ echo "Quarter $quarter"
 echo "Total batch index $totallines"
 
 start=1
-end=101
+end=51
 
 while [ $start -le $totallines ]
 do
   echo "qsub -v 'quarter=$1,batch_start=$start,batch_end=$end' pbs_quarter_batch.sh"
   qsub -v "quarter=$1,batch_start=$start,batch_end=$end" pbs_quarter_batch.sh
 
-  start=$(($start + 100))
-  end=$(($end + 100))
+  start=$(($start + 50))
+  end=$(($end + 50))
   if [ "$end" -ge $totallines ]
   then
     end=$totallines
