3c82aba55264307095302b2572b82bb15318712d
Jorge <jorgemarpa@ug.uchile.cl>
2022-07-27 17:18:47 -0700

    more mem

--- bash/pbs_quarter_channel.sh
+++ bash/pbs_quarter_channel.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/sh
 #PBS -N make-LCs
 #PBS -q normal
-#PBS -l select=1:ncpus=5:mem=60G:model=ivy
+#PBS -l select=1:ncpus=5:mem=62G:model=ivy
 #PBS -l walltime=07:00:00
 #PBS -j oe
 #PBS -m e
