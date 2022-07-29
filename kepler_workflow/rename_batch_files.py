62a5e7ef849f2192bc806101a1a22b4e06fc317d
Jorge <jorgemarpa@ug.uchile.cl>
2022-07-29 14:25:10 -0700

    prints

--- kepler_workflow/rename_batch_files.py
+++ kepler_workflow/rename_batch_files.py
@@ -11,13 +11,13 @@ suffix = "fvaT_bkgF_augT_sgmF_iteT_cbvT"
 version = "1.1.1"
 
 for ch in channels:
-    in_files = sorted(
-        glob(
-            f"{LCS_PATH}/kepler/ch{ch:02}/q{quarter:02}/"
-            f"kbonus-kepler-bkg_ch{ch:02}_q{quarter:02}_"
-            f"v{version}*_{suffix}.*"
-        )
+    pattern = (
+        f"{LCS_PATH}/kepler/ch{ch:02}/q{quarter:02}/"
+        f"kbonus-kepler-bkg_ch{ch:02}_q{quarter:02}_"
+        f"v{version}*_{suffix}.*"
     )
+    print(pattern)
+    in_files = sorted(glob(pattern))
     print(f"Total files {len(in_files)}")
     for fi in tqdm(in_files, total=len(in_files)):
         fo = fi.replace("bkgF", "bkgT")
