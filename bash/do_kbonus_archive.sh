2efb1e20c7053a27cc4d70cedd531e8b74ac2cc2
Jorge <jorgemarpa@ug.uchile.cl>
2022-08-29 22:03:38 -0700

    do final archive

---
+++ bash/do_kbonus_archive.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+quarter=$1
+
+echo "Creating tarball archive..."
+seq 1 10 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 11 20 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 21 30 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 31 40 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 41 50 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 51 60 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 61 70 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+seq 71 84 | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python kbonus_lcf_archive.py --dir {} --delete
+
+echo "Creating source catalog"
+cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python do_catalog.py --quarter $quarter --dir {}
+
+python do_catalog.py --quarter 5 --concat
