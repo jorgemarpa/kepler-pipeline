40ff698c1b7babcb67ceb757995c3083943063fe
Jorge <jorgemarpa@ug.uchile.cl>
2022-08-29 22:36:38 -0700

    removing empty firs

--- bash/do_kbonus_archive.sh
+++ bash/do_kbonus_archive.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 quarter=$1
-kwpath="/home4/jimartin/ADAP/kepler-workflow/kepler_workflow"
+kwpath="/home4/jimartin/ADAP/kepler-workflow"
 
 echo "Creating tarball archive..."
-seq 1 10 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 1 10 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 11 20 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 11 20 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 21 30 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 21 30 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 31 40 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 31 40 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 41 50 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 41 50 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 51 60 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 51 60 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 61 70 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 61 70 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
-seq 71 84 | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --quarter $quarter --channel {}
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kbonus_lcf_archive.py --dir {} --delete
+seq 71 84 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
 
 echo "Creating source catalog"
-cat ../data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/do_catalog.py --quarter $quarter --dir {}
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/do_catalog.py --quarter $quarter --dir {}
 
-python $kwpath/do_catalog.py --quarter 5 --concat
+python $kwpath/kepler_workflow/do_catalog.py --quarter 5 --concat
