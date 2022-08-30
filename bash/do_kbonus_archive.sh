1058534e20cdb5238af1621ea37a877d862b640a
Jorge <jorgemarpa@ug.uchile.cl>
2022-08-29 22:41:48 -0700

    removing empty dirs

--- bash/do_kbonus_archive.sh
+++ bash/do_kbonus_archive.sh
@@ -6,27 +6,35 @@ kwpath="/home4/jimartin/ADAP/kepler-workflow"
 echo "Creating tarball archive..."
 seq 1 10 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 11 20 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 21 30 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 31 40 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 41 50 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 51 60 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 61 70 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 seq 71 84 | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --quarter $quarter --channel {}
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/kbonus_lcf_archive.py --dir {} --delete
+cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -P 5 -l bash -c 'rm -r /nobackupp19/jimartin/ADAP/kbonus/lcs/kepler/$0'
 
 echo "Creating source catalog"
 cat $kwpath/data/support/kbonus_archive_id4.dat | xargs -n 1 -P 5 -I {} python $kwpath/kepler_workflow/do_catalog.py --quarter $quarter --dir {}
