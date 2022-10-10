0d6f9785e2dc121937039cb41683d2640192926d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 10:23:19 +0000

    Adding tarfile member sanitization to extractall()

--- kepler_workflow/create_kepler_lcf_archive.py
+++ kepler_workflow/create_kepler_lcf_archive.py
@@ -17,7 +17,26 @@ def main():
             os.mkdir(tmp_dir)
         print(f"Unpacking {tf}")
         with tarfile.open(tf) as tar:
-            tar.extractall(tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, tmp_dir)
 
         fits_list = glob.glob(f"{ARCHIVE_PATH}/data/kepler/lcs/q05/tmp/kplr*_llc.fits")
         kics = [x.split("/")[-1].split("-")[0][4:] for x in fits_list]
